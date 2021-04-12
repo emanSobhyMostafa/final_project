4a74b5650b3318fde33cfdf875be30992332e0a5
Abo-ghazalla <mohamedabughazalla205@gmail.com>
2021-04-12 16:41:46 +0200

    test2

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text("test"),
+      ),
+    );
   }
 }
