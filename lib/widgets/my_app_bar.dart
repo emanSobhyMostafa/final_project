1bf22cf14fa688b07feac4ddc7d1ce076bd4c093
Sameh Mohamed <sameh.abdelhaleem95@gmail.com>
2021-04-12 21:26:21 +0200

    get rid of extra code at MyAppBar

--- lib/widgets/my_app_bar.dart
+++ lib/widgets/my_app_bar.dart
@@ -1,41 +1,5 @@
 import 'package:flutter/material.dart';
 
-// class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-//   final Color backgroundColor = Colors.red;
-
-//   final double height;
-
-//   /// you can add more fields that meet your needs
-
-//   const MyAppBar({Key key, this.height}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AppBar(
-//       title: Container(
-//         height: height,
-//         child: Column(
-//           children: [
-//             Text('LOGO'),
-//             TextField(
-//               decoration: InputDecoration(
-//                   hintText: 'Search',
-//                   prefixIcon: Icon(Icons.search),
-//                   border: OutlineInputBorder(),
-//                   focusColor: Colors.white),
-//             )
-//           ],
-//         ),
-//       ),
-//       backgroundColor: backgroundColor,
-//       actions: [],
-//     );
-//   }
-
-//   @override
-//   Size get preferredSize => new Size.fromHeight(height);
-// }
-
 class MyAppBar extends PreferredSize {
   final double height;
 
@@ -69,27 +33,3 @@ class MyAppBar extends PreferredSize {
   }
 }
 
-// return Container(
-//       height: preferredSize.height,
-//       color: Colors.black87,
-//       alignment: Alignment.center,
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Padding(
-//               padding: const EdgeInsets.only(left: 8),
-//               child:Row(children: [ Drawer,Text(
-//                 'LOGO',style: TextStyle(
-//                   fontSize: 24,
-//                   color: Colors.white
-//                   )
-//                 )],),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: 
-//             )
-//           ],
-//         ),
-//     );
