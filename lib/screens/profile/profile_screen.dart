6bdacd276e4a70b7ecfbd09c294e36391e178382
Eman Sobhy <engemansoby@gmail.com>
2021-04-23 13:36:09 +0200

    profile

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:final_project/config/theme_colors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +15,34 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-          children: [Text("Your Email Is:" + auth.currentUser.email)],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "You Logged In with:" + auth.currentUser.displayName,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text("Your Email Is:" + auth.currentUser.email),
+              SizedBox(
+                height: 25,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: third,
+                  onPrimary: white,
+                  elevation: 5.0,
+                ),
+                child: Text(
+                  'Logout',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
